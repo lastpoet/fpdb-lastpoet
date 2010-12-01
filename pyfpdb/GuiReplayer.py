db812f147911bc77bd6b3169de4b048465282d98
lastpoet <maxime@grandchamp.net>
2010-12-01 13:24:43 +0000

    first stab at the replayer

--- pyfpdb/GuiReplayer.py
+++ pyfpdb/GuiReplayer.py
@@ -95,53 +95,81 @@ class GuiReplayer:
         
         self.maxseats=self.MyHand.maxseats
         
-        self.table={}     #create table with positions, player names and stacks
+        if self.MyHand.gametype['currency']=="USD":    #TODO: check if there are others ..
+            self.currency="$"
+        elif self.MyHand.gametype['currency']=="EUR":
+            self.currency="€"
+            
+                
+        self.table={}     #create table with positions, player names, status (live/folded), stacks and chips on table
         for i in range(0,self.maxseats):     # radius: 200, center: 250,250
             x= int (round(250+200*math.cos(2*i*math.pi/self.maxseats)))
             y= int (round(250+200*math.sin(2*i*math.pi/self.maxseats)))            
             try:
-                self.table[i]={"name":self.MyHand.players[i][1],"stack":Decimal(self.MyHand.players[i][2]),"x":x,"y":y}               #save coordinates of each player          
+                self.table[i]={"name":self.MyHand.players[i][1],"stack":Decimal(self.MyHand.players[i][2]),"x":x,"y":y,"chips":0,"status":"live"}               #save coordinates of each player
+                try:
+                    self.table[i]['holecards']=self.MyHand.holecards["PREFLOP"][self.MyHand.players[i][1]][1]+' '+self.MyHand.holecards["PREFLOP"][self.MyHand.players[i][1]][2]
+                    print "holecards",self.table[i]['holecards']
+                except:
+                    self.table[i]['holecards']=''
             except IndexError:  #if seat is empty
-                print "seat",i+1,"out of",self.maxseats,"empty"
-                pass  
+                print "seat",i+1,"out of",self.maxseats,"empty"                  
 
         self.actions=[]     #create list with all actions
-              
+                        
         if isinstance(self.MyHand, HoldemOmahaHand):
             if self.MyHand.gametype['category'] == 'holdem':
                 self.play_holdem()
 
-        self.action_number=0                
+        self.action_number=0
+        self.action_level=0 
+        self.pot=0               
         gobject.timeout_add(1000,self.draw_action)
        
                     
     def area_expose(self, area, event):
-        print "expose action number",self.action_number 
         self.style = self.area.get_style()
         self.gc = self.style.fg_gc[gtk.STATE_NORMAL]
 
         playerid='999'  #makes sure we have an error if player is not recognised
-        for i in range(0,len(self.table)):  #must be a better way to find the player id in the table...
+        for i in range(0,len(self.table)):  #surely there must be a better way to find the player id in the table...
             if self.table[i]['name']==self.actions[self.action_number][1]:
                 playerid=i
+                
+        if self.actions[self.action_number][2]=="folds":
+            self.table[playerid]["status"]="folded"
         
         if self.actions[self.action_number][3]:
             self.table[playerid]["stack"] -= Decimal(self.actions[self.action_number][3])  #decreases stack if player bets
+            self.pot += Decimal(self.actions[self.action_number][3]) #increase pot
+            self.table[playerid]["chips"] += Decimal(self.actions[self.action_number][3]) #increase player's chips on table
+
                 
-        cm = self.gc.get_colormap()
-        color = cm.alloc_color("black")                      
+        cm = self.gc.get_colormap() #create colormap toi be able to play with colours
+
+        color = cm.alloc_color("black") #defaults to black                     
         self.gc.set_foreground(color)   
         
         self.area.window.draw_arc(self.gc, 0, 125, 125, 300, 300, 0, 360*64) #table
+        
         for i in self.table:
-            self.pangolayout.set_text(self.table[i]["name"])     #player names 
+            if self.table[i]["status"]=="folded":
+                color = cm.alloc_color("grey") #player has folded => greyed out                     
+                self.gc.set_foreground(color) 
+            else:
+                color = cm.alloc_color("black") #player is live                     
+                self.gc.set_foreground(color)          
+            self.pangolayout.set_text(self.table[i]["name"]+self.table[i]["holecards"])     #player names + holecards 
             self.area.window.draw_layout(self.gc, self.table[i]["x"],self.table[i]["y"], self.pangolayout)                                 
             self.pangolayout.set_text('$'+str(self.table[i]["stack"]))     #player stacks
-            self.area.window.draw_layout(self.gc, self.table[i]["x"]+10,self.table[i]["y"]+20, self.pangolayout)
+            self.area.window.draw_layout(self.gc, self.table[i]["x"]+10,self.table[i]["y"]+20, self.pangolayout)            
             
         color = cm.alloc_color("green")                      
         self.gc.set_foreground(color)  
-                        
+        
+        self.pangolayout.set_text(self.currency+str(self.pot)) #displays pot
+        self.area.window.draw_layout(self.gc,270,270, self.pangolayout)
+                    
         if self.actions[self.action_number][0]>1:   #displays flop
             self.pangolayout.set_text(self.MyHand.board['FLOP'][0]+" "+self.MyHand.board['FLOP'][1]+" "+self.MyHand.board['FLOP'][2])
             self.area.window.draw_layout(self.gc,210,240, self.pangolayout)
@@ -157,8 +185,9 @@ class GuiReplayer:
                        
         self.pangolayout.set_text(self.actions[self.action_number][2]) #displays action       
         self.area.window.draw_layout(self.gc, self.table[playerid]["x"]+10,self.table[playerid]["y"]+35, self.pangolayout)
-        self.pangolayout.set_text(self.actions[self.action_number][3]) #displays amount   
-        self.area.window.draw_layout(self.gc, self.table[playerid]["x"]+10,self.table[playerid]["y"]+55, self.pangolayout)
+        if self.actions[self.action_number][3]:  #displays amount  
+            self.pangolayout.set_text(self.currency+self.actions[self.action_number][3])  
+            self.area.window.draw_layout(self.gc, self.table[playerid]["x"]+10,self.table[playerid]["y"]+55, self.pangolayout)
 
         color = cm.alloc_color("black")      #we don't want to draw the filters and others in red                
         self.gc.set_foreground(color)    
@@ -173,20 +202,32 @@ class GuiReplayer:
                     amount=str(self.MyHand.actions[action][i][2])
                 except:
                     amount=''   #no amount
-                self.actions.append([actions.index(action),player,act,amount])
-        print self.actions
+                self.actions.append([actions.index(action),player,act,amount])  #create table with all actions
         
 
     def draw_action(self):
-        if self.action_number==len(self.actions)-1:
+        if self.action_number==len(self.actions)-1:     #no more actions, we exit the loop
             return False
+    
+        if self.actions[self.action_number][0]!=self.action_level:  #have we changed street ?
+            self.action_level=self.actions[self.action_number][0] #record the new street
+            if self.action_level>1: #we don't want to refresh if simply moving from antes/blinds to preflop action
+                alloc = self.area.get_allocation()
+                rect = gtk.gdk.Rectangle(0, 0, alloc.width, alloc.height)   
+                self.area.window.invalidate_rect(rect, True)    #make sure we refresh the whole screen        
+                        
         self.action_number+=1
         if self.area.window:
-            alloc = self.area.get_allocation()
-            rect = gtk.gdk.Rectangle(0, 0, alloc.width, alloc.height)
-            self.area.window.invalidate_rect(rect, True)
+            playerid='999'  #makes sure we have an error if player is not recognised
+            for i in range(0,len(self.table)):  #surely there must be a better way to find the player id in the table...
+                if self.table[i]['name']==self.actions[self.action_number][1]:
+                    playerid=i
+                    rect = gtk.gdk.Rectangle(self.table[playerid]["x"],self.table[playerid]["y"],100,100)
+                    self.area.window.invalidate_rect(rect, True)    #refresh player area of the screen
+                    rect = gtk.gdk.Rectangle(270,270,100,50)
+                    self.area.window.invalidate_rect(rect, True)    #refresh pot area                   
             self.area.window.process_updates(True)
-        print "draw action",self.action_number
+        print "draw action",self.action_number,self.actions[self.action_number][1],self.actions[self.action_number][2],self.actions[self.action_number][3]
         return True
 
 
